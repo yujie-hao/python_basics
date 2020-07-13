7321fa1f2417799aed877a8a40aaa97d4a81bb65
Yujie Hao <haoyj20@gmail.com>
2020-07-13 02:04:59 -0400

    added operators.py

---
+++ datatypes/numbers.py
@@ -0,0 +1,14 @@
+a = 5
+print(a, "is of type", type(a))
+
+a = -1.1
+print(a, "is of type", type(a))
+
+a = 9.7e3  # scientific notation
+print(a, "is of type", type(a))
+
+a = 1e-1
+print(a, "is of type", type(a))
+
+a = 0xff  # 255
+print(a, "is of type", type(a))
---
+++ operators/operators.py
@@ -0,0 +1,29 @@
+# Reference: https://www.programiz.com/python-programming/operators
+
+# ============================================
+# Arithmetic
+print('### Arithmetic operators ###\n')
+# divide I: results into float
+a = 5 / 2
+print("5 / 2 = ", a)
+
+# divide II: results into whole number
+a = 5.0 // 2
+print("5.0 // 2 = ", a)
+
+a = 5 // 2.0
+print("5 // 2.0 = ", a)
+
+# exponent
+a = 5 ** 3
+print("5 ** 3 = ", a)
+
+# ============================================
+# Logical operators
+print('\n### Logical operators ###\n')
+x = True
+y = False
+
+print('x and y = ', x and y)
+print('x or y = ', x or y)
+print('not x = ', not x)
