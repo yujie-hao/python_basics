d73ab7b6deb8ec324d40cad5ea0423efb25fe062
Yujie Hao <haoyj20@gmail.com>
2020-07-21 02:02:38 -0400

    Added tuple.py

---
+++ data_structure/tuple.py
@@ -0,0 +1,23 @@
+"""
+Tuple is similar to a list
+
+Tuple: element is immutable
+list: element is mutable
+
+"""
+
+tuple_1 = ()
+print(tuple_1)
+
+tuple_1 = (1, 2, 3)
+print(tuple_1)
+
+tuple_1 = ("string1", bool, 1.23, ['a', 1], ('b', 2))
+print(tuple_1)
+
+tuple_1 = [1, 'a'], 3.14, False, tuple_1
+print(tuple_1)
+
+print(tuple_1[0][1])
+print(tuple_1[-1][-2])
+print(tuple_1[-1][-2][1])
