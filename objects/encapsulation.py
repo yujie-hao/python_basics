4838653cfc3159bd62911edfc93c2ee418a7db8d
Yujie Hao <haoyj20@gmail.com>
2020-11-08 02:48:15 -0500

    Added property into encapsulation.py

--- objects/encapsulation.py
+++ objects/encapsulation.py
@@ -20,3 +20,23 @@ c.sell()
 # using the setter member function
 c.setMaxPrice(1000)
 c.sell()
+
+
+# Property
+class Smartphone:
+    def __init__(self, value):
+        self._price = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        assert isinstance(value, int)
+        assert value >= 0 and value <= 1500
+
+
+iPhone = Smartphone()
+iPhone.price = 1000
+print("iPhone.price: " + iPhone.price.__str__())
